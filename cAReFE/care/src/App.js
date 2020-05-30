206df4910262f6b3fca5963471a0475e5e1f2035
ShayaanAkbar <shayaanakbar1106@gmail.com>
2020-05-30 08:57:24 -0500

    Stil creating shell

---
+++ cAReFE/care/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+
+function App() {
+  return (
+      <div>
+        <h2>Hello</h2>
+      </div>
+  )
+}
+
+export default App
