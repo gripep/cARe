881e64486b058e240bebc54c73934f4ededd22dc
ShayaanAkbar <shayaanakbar1106@gmail.com>
2020-05-30 09:28:48 -0500

    fixing app.js

--- cAReFE/care/src/App.js
+++ cAReFE/care/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+import "./assets/vendor/nucleo/css/nucleo.css";
+import "./assets/vendor/font-awesome/css/font-awesome.min.css";
+import "./assets/scss/argon-design-system-react.scss";
 
 function App() {
-  return (
-      <div>
-        <h2>Hello</h2>
-      </div>
-  )
+    return (
+        <div>
+            <h2>Hello</h2>
+        </div>
+    );
 }
-
-export default App
+export default App;
