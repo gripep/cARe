37e05dea5b576c425f60ab8b2d56dba54aba1637
gripepi <gian.ripepi96@gmail.com>
2020-05-30 17:52:01 +0100

    Added Footer

--- cAReFE/care/src/App.js
+++ cAReFE/care/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
+
 import Body from "./components/Body/Body";
+import Footer from "./components/Footer/Footer";
 
 import "./assets/css/argon-design-system-react.min.css";
 import "./assets/css/argon-design-system-react.css.map";
 
 function App() {
-    return (
-        <div>
-            <Body />
-        </div>
-    );
+  return (
+    <div>
+      <Body />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
