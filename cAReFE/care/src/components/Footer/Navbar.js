ab2d82c1c4c9363b7d7d7563a93e5d9ea9cbf30b
gripepi <gian.ripepi96@gmail.com>
2020-05-30 18:39:04 +0100

    Added tooltips to navbar

--- cAReFE/care/src/components/Footer/Navbar.js
+++ cAReFE/care/src/components/Footer/Navbar.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
+// reactstrap components
+import { UncontrolledTooltip } from "reactstrap";
+
 // react-fontawesome icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faRssSquare } from "@fortawesome/free-solid-svg-icons";
@@ -15,30 +18,52 @@ export class Navbar extends Component {
               <div className="col-4 col-md-auto">
                 <ul class="navbar-nav mr-auto">
                   <li>
-                    <a className="nav-link mr-5 ml-5" href="#">
+                    <a className="nav-link mr-5 ml-5" id="home" href="#">
                       <FontAwesomeIcon icon={faHome} size="3x" />
                       <span className="nav-link-inner--text d-lg-none ml-2">
                         Home
                       </span>
+                      <UncontrolledTooltip
+                        delay={0}
+                        placement="top"
+                        target="home"
+                      >
+                        Home
+                      </UncontrolledTooltip>
                     </a>
                   </li>
                   <li>
-                    <a className="nav-link mr-5 ml-5" href="#">
+                    <a className="nav-link mr-5 ml-5" id="news" href="#">
                       <FontAwesomeIcon icon={faRssSquare} size="3x" />
                       <span className="nav-link-inner--text d-lg-none ml-2">
                         News
                       </span>
+                      <UncontrolledTooltip
+                        delay={0}
+                        placement="top"
+                        target="news"
+                      >
+                        COVID-19 News
+                      </UncontrolledTooltip>
                     </a>
                   </li>
                   <li>
                     <a
                       className="nav-link mr-5 ml-5"
+                      id="github"
                       href="https://github.com/gripep/cARe"
                     >
                       <FontAwesomeIcon icon={faGithub} size="3x" />
                       <span className="nav-link-inner--text d-lg-none ml-2">
                         GitHub
                       </span>
+                      <UncontrolledTooltip
+                        delay={0}
+                        placement="top"
+                        target="github"
+                      >
+                        Check out our repo!
+                      </UncontrolledTooltip>
                     </a>
                   </li>
                 </ul>
