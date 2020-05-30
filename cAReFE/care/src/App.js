3abede334d4e20216c7c01736b5e25cab32075e4
gripepi <gian.ripepi96@gmail.com>
2020-05-30 16:07:26 +0100

    Fixed font-awesome bug

--- cAReFE/care/src/App.js
+++ cAReFE/care/src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-import "./assets/vendor/nucleo/css/nucleo.css";
-import "./assets/vendor/font-awesome/css/font-awesome.min.css";
-import "./assets/scss/argon-design-system-react.scss";
+// import "./assets/vendor/nucleo/css/nucleo.css";
+// import "./assets/vendor/font-awesome/css/font-awesome.min.css";
+// import "./assets/scss/argon-design-system-react.scss";
+import "./assets/css/argon-design-system-react.min.css";
+import "./assets/css/argon-design-system-react.css.map";
 
 function App() {
-    return (
-        <div>
-            <h2>Hello</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Hello</h2>
+    </div>
+  );
 }
 
 export default App;
