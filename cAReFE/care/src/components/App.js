620da2a460d02f047d6e7763e94acb8956e43294
gripepi <gian.ripepi96@gmail.com>
2020-05-30 14:16:35 +0100

    Setup App.js && index.js

---
+++ cAReFE/care/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+// import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "../assets/vendor/nucleo/css/nucleo.css";
+import "../assets/vendor/font-awesome/css/font-awesome.min.css";
+import "../assets/scss/argon-design-system-react.scss?v1.1.0";
+
+export class App extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
