35b5a6927d0c853d2a86203ead38edb2c026a3fa
gripepi <gian.ripepi96@gmail.com>
2020-05-30 20:11:09 +0100

    Fixed bug && Added header component & routing

--- cAReFE/care/src/components/Footer/Navbar.js
+++ cAReFE/care/src/components/Footer/Navbar.js
@@ -11,14 +11,14 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 export class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg fixed-bottom navbar-light bg-primary">
+      <nav className="navbar navbar-expand-lg fixed-bottom navbar-dark bg-primary">
         <div class="collapse navbar-collapse">
           <div className="container-fluid">
             <div className="row justify-content-md-center">
               <div className="col-4 col-md-auto">
                 <ul class="navbar-nav mr-auto">
                   <li>
-                    <a className="nav-link mr-5 ml-5" id="home" href="#">
+                    <a className="nav-link mr-5 ml-5" id="home" href="/">
                       <FontAwesomeIcon icon={faHome} size="3x" />
                       <span className="nav-link-inner--text d-lg-none ml-2">
                         Home
@@ -33,8 +33,7 @@ export class Navbar extends Component {
                     </a>
                   </li>
                   <li>
-
-                    <a className="nav-link mr-5 ml-5" href='cAReFE/care/src/components/Body/News.js'>
+                    <a className="nav-link mr-5 ml-5" id="news" href="/news">
                       <FontAwesomeIcon icon={faRssSquare} size="3x" />
                       <span className="nav-link-inner--text d-lg-none ml-2">
                         News
